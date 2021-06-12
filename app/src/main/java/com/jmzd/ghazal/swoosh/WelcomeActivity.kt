3dfd3db6f3aaa0cb3a450601642725a771390710
Your Name <jmzd.developer94@gmail.com>
2021-06-12 19:06:58 +0430

    SetOnClickListener for WelcomeActivity.kt

--- app/src/main/java/com/jmzd/ghazal/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/swoosh/WelcomeActivity.kt
@@ -1,5 +1,6 @@
 package com.jmzd.ghazal.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jmzd.ghazal.swoosh.databinding.ActivityWelcomeBinding
@@ -11,6 +12,9 @@ class WelcomeActivity : AppCompatActivity() {
         val binding = ActivityWelcomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
+        binding.getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
