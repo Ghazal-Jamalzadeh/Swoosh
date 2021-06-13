cc7a4dbbaabda38750128ae1840f5bad44a5da8c
Your Name <jmzd.developer94@gmail.com>
2021-06-13 16:39:10 +0430

    LeagueActivity

--- app/src/main/java/com/jmzd/ghazal/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/jmzd/ghazal/swoosh/LeagueActivity.kt
@@ -1,11 +1,22 @@
 package com.jmzd.ghazal.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.jmzd.ghazal.swoosh.databinding.ActivityLeagueBinding
+import com.jmzd.ghazal.swoosh.databinding.ActivityWelcomeBinding
 
 class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+        val binding = ActivityLeagueBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.leagueNextBtn.setOnClickListener{
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            startActivity(skillActivity)
+        }
     }
 }
